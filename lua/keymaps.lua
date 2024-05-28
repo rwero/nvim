ef34a3973b5f7bc06e4acd2a71887ee44ee43cd7
Redouane Moussaoui <RedouaneMoussaoui@protonmail.com>
2024-05-28 14:42:13 +0100

    set up neovim with basic config

---
+++ lua/keymaps.lua
@@ -0,0 +1,37 @@
+vim.keymap.set("n", "<C-h>", "<C-w>h" )
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+-- move lines up and down
+vim.keymap.set("n", "<A-j>", ":m +1<CR>")
+vim.keymap.set("n", "<A-k>", ":m -2<CR>")
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-Up>", ":resize -2<CR>")
+vim.keymap.set("n", "<C-Down>", ":resize +2<CR>")
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>")
+-- Resize with arrows
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>")
+
+-- Navigate buffers
+vim.keymap.set("n", "<S-l>", ":bnext<CR>")
+vim.keymap.set("n", "<S-h>", ":bprevious<CR>")
+
+vim.keymap.set("n", "<leader>bd", "<cmd>:bd<cr>")
+
+-- Clear highlights
+vim.keymap.set("n", "<leader>h", "<cmd>nohlsearch<CR>")
+
+-- Close buffers
+vim.keymap.set("n", "<S-q>", "<cmd>Bdelete!<CR>")
+
+-- Insert --
+-- Press jk fast to enter
+vim.keymap.set("i", "jk", "<ESC>")
+
+-- Visual --
+-- Stay in indent mode
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,16 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+		"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	config = function()
+		require("neo-tree").setup({
+			close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+			popup_border_style = "rounded",
+		})
+		vim.keymap.set("n", "<leader>e", ":Neotree toggle <CR>")
+	end,
+}
